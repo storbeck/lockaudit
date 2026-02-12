b0a642644c1a4d1ad4f255cc38b577c3b3d598db
Geoff Storbeck <geoff@pagefoundry.dev>
2026-02-11 19:44:10 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -14,28 +14,13 @@ async function run(filePath) {
 
   console.log("Done.")
 
-  const countsResult = await conn.query(`
-    MATCH (proj:Project) RETURN count(proj) AS projects
-  `)
-  const packagesResult = await conn.query(`
-    MATCH (p:Package) RETURN count(p) AS package_versions
-  `)
-  const lockPackagesResult = await conn.query(`
-    MATCH (lp:LockPackage) RETURN count(lp) AS lock_packages
-  `)
-  const depsResult = await conn.query(`
-    MATCH (d:DependencyDecl) RETURN count(d) AS dependency_decls
+  const result = await conn.query(`
+    MATCH (p:Package)
+    RETURN count(p) as total
   `)
 
-  const projects = (await countsResult.getAll())[0].projects
-  const packageVersions = (await packagesResult.getAll())[0].package_versions
-  const lockPackages = (await lockPackagesResult.getAll())[0].lock_packages
-  const dependencyDecls = (await depsResult.getAll())[0].dependency_decls
-
-  console.log("Projects:", projects)
-  console.log("Package versions:", packageVersions)
-  console.log("Lockfile package instances:", lockPackages)
-  console.log("Dependency declarations:", dependencyDecls)
+  const rows = await result.getAll()
+  console.log("Total packages:", rows[0].total)
 }
 
 module.exports = { run }
